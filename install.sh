ecd6c1c8c1550b01f65af70a4851fac81708b963
Prakhar Gupta <95362168+prakharguptaujjain@users.noreply.github.com>
2022-12-22 17:26:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,6 @@ if [ -f /etc/apt/sources.list ]; then
   # Remove all lines containing "kali.org" from the file
   sudo grep -v "kali.org" /etc/apt/sources.list > /tmp/sources.list
   sudo mv /tmp/sources.list /etc/apt/sources.list
-
-  # Update the package list
-  sudo apt update
 else
   echo "Error: /etc/apt/sources.list not found"
 fi
