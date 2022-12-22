3cb5e28fdc604eb81d20ccf78949e7701024df53
Prakhar Gupta <95362168+prakharguptaujjain@users.noreply.github.com>
2022-12-22 16:58:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list
 name=$(curl -s https://http.kali.org/kali/pool/main/k/kali-archive-keyring/ | grep "all.deb" | awk -F'[<>]' '{for (i=1; i<=NF; i++) print $i}' | grep "all.deb" | sed -n '2p')
 wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/$name
-sudo apt install $name
+sudo apt install ./$name
 sudo apt update
 rm $name
 
